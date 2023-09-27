b0a42c25daf50f6ff5b973541e5d7b2515a60abf
Nancy Everest <neverest@us.ci.org>
2023-09-26 21:10:10 -0700

    read file from command line argument

--- tokki.go
+++ tokki.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+// main function, reads filename from second argument
+// usage: go run . sample.tk
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+	contents, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	fmt.Println("Contents of file:", string(contents))
 }
