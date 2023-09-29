9479c48db969fe7c0b871ecc6731ed10e2e87a59
trvrenglish <trvrenglish@gmail.com>
2023-09-28 22:03:43 -0700

    getChar function

--- tokki.go
+++ tokki.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"io/ioutil" // not sure if we need this, but we might for input and output patterns 
 )
 
 // main function, reads filename from second argument
@@ -17,40 +16,46 @@ func main() {
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
+	} else {
+		getChar(contents)
+
 	}
 	fmt.Println("Contents of file:", string(contents))
 }
 
-// Function decs were here in the book but in Go functions cant be defined without a body so I think we can just declare and define them later on
+// getChar - A Function to get the next character of input and determine its character class
+func getChar(contents []byte) {
+	nextChar = []byte(contents)[0]
+	fmt.Printf(string(nextChar))
+}
 
-// Global Declaration Variables 
+// Global Declaration Variables
 var (
-	charClass int 
-	lexeme [100]byte
-	nextChar byte
-	lexLen int 
-	token int
-	nextToken int 
-	inputSource string 
+	charClass   int
+	lexeme      [100]byte
+	nextChar    byte
+	lexLen      int
+	token       int
+	nextToken   int
+	inputSource string
 )
-// Character Classes 
+
+// Character Classes
 const (
-	LETTER = 0
-	DIGIT = 1 
+	LETTER  = 0
+	DIGIT   = 1
 	UNKNOWN = 99
 )
 
-// Token Codes 
-const ( 
-	INT_LIT = 10 
-	IDENT = 11
-	ASSIGN_OP = 20
-	ADD_OP = 21
-	SUB_OP = 22
-	MULT_OP = 23
-	DIV_OP = 24
-	LEFT_PAREN = 25
+// Token Codes
+const (
+	INT_LIT     = 10
+	IDENT       = 11
+	ASSIGN_OP   = 20
+	ADD_OP      = 21
+	SUB_OP      = 22
+	MULT_OP     = 23
+	DIV_OP      = 24
+	LEFT_PAREN  = 25
 	RIGHT_PAREN = 26
 )
-
-
