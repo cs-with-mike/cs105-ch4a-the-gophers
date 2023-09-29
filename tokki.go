1a5601fb7fbcdcb75d84c5ea4409505804d718dd
aL pete <alpete@aLs-MacBook-Air.local>
2023-09-28 19:42:03 -0700

    Global Variables Declared

--- tokki.go
+++ tokki.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"io/ioutil" // not sure if we need this, but we might for input and output patterns 
 )
 
 // main function, reads filename from second argument
@@ -19,3 +20,14 @@ func main() {
 	}
 	fmt.Println("Contents of file:", string(contents))
 }
+
+// Global Declarations 
+var (
+	charClass int 
+	lexeme [100]byte
+	nextChar byte
+	lexLen int 
+	token int
+	nextToken int 
+	inputSource string 
+)
