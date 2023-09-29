0adc0e98a46d9c34d180e532b02f6db6f5a8e3e1
trvrenglish <trvrenglish@gmail.com>
2023-09-28 21:04:25 -0700

    Changed commands from Python to Go

--- tokki.sh
+++ tokki.sh
@@ -9,11 +9,11 @@
 echo "Setting up runtime ..."
 
 # I'm checking Python interpreter's version here just as a placeholder.
-python3 --version # TODO: YOUR RUNTIME SETUP HERE.
+go version # TODO: YOUR RUNTIME SETUP HERE.
 
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
 echo "Running Tokki ..."
 
 # As an example, I have provided how I would run my tokki.pyc.
-python3 tokki.pyc $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
+go run tokki.go $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
