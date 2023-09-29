5fbfe001d34e81a0a8e80b9d33b51201e9cc1c12
aL pete <alpete@aLs-MacBook-Air.local>
2023-09-28 20:18:03 -0700

    Character Classes and Token Codes added

--- tokki.go
+++ tokki.go
@@ -21,7 +21,9 @@ func main() {
 	fmt.Println("Contents of file:", string(contents))
 }
 
-// Global Declarations 
+// Function decs were here in the book but in Go functions cant be defined without a body so I think we can just declare and define them later on
+
+// Global Declaration Variables 
 var (
 	charClass int 
 	lexeme [100]byte
@@ -31,3 +33,24 @@ var (
 	nextToken int 
 	inputSource string 
 )
+// Character Classes 
+const (
+	LETTER = 0
+	DIGIT = 1 
+	UNKNOWN = 99
+)
+
+// Token Codes 
+const ( 
+	INT_LIT = 10 
+	IDENT = 11
+	ASSIGN_OP = 20
+	ADD_OP = 21
+	SUB_OP = 22
+	MULT_OP = 23
+	DIV_OP = 24
+	LEFT_PAREN = 25
+	RIGHT_PAREN = 26
+)
+
+
