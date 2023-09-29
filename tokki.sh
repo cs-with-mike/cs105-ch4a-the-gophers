fefe4277f082301853bc5f88c6598fae719da262
trvrenglish <trvrenglish@gmail.com>
2023-09-29 13:25:39 -0700

    env test

--- tokki.sh
+++ tokki.sh
@@ -10,12 +10,13 @@ echo "Setting up runtime ..."
 
 # I'm checking Go interpreter's version here just as a placeholder.
 go version # TODO: YOUR RUNTIME SETUP HERE.
-chmod +x tokki.go
-which env
-type env
+
+//usr/bin/env go run $0 "$@"; exit
+chmod 744 tokki.go
+
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
 echo "Running Tokki ..."
 
 # As an example, I have provided how I would run my tokki.pyc.
-./tokki.go $1 > out.txt
+./tokki.go sample.tk > out.txt
