3d2eb02d2c3ddf8f46bc3a847b6bb439d12ae534
Mike Ryu <mryu@westmont.edu>
2023-09-29 19:32:04 -0700

    Attempt #2: GOCACHE must be an absolute path (makes sense ...). -Ryu

--- tokki.sh
+++ tokki.sh
@@ -12,7 +12,8 @@ echo "Setting up runtime ..."
 sudo apt install golang; go version
 
 # Setup GOCACHE environment variable. (mryu)
-mkdir ./go_cache && export GOCACHE=./go_cache
+echo "Creating directory $(pwd)/go_cache and setting it to GOCACHE envvar."
+mkdir ./go_cache && export GOCACHE=$(pwd)/go_cache
 
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
