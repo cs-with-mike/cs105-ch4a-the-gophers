5b6ae64c1f7c1718e9f7bb4321970b18addb2621
Mike Ryu <mryu@westmont.edu>
2023-09-29 19:34:01 -0700

    Attempt #3: Not letting mkdir fail on subsequent runs. -Ryu

--- tokki.sh
+++ tokki.sh
@@ -13,7 +13,7 @@ sudo apt install golang; go version
 
 # Setup GOCACHE environment variable. (mryu)
 echo "Creating directory $(pwd)/go_cache and setting it to GOCACHE envvar."
-mkdir ./go_cache && export GOCACHE=$(pwd)/go_cache
+mkdir -p ./go_cache; export GOCACHE=$(pwd)/go_cache
 
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
